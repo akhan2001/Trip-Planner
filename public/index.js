ca3b41bbdbd49d62cf4b5ffbcee0f8717845dfc5
Abdullah Khan <47902386+akhan2001@users.noreply.github.com>
2021-07-04 12:15:46 -0700

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,9 @@
+//this is client code
+// Put all onload AJAX calls here, and event listeners
+jQuery(document).ready(function() {
+    //testing function
+    function addWaypoint() {
+        document.getElementById("demo").innerHTML = "sent successful event action";
+    }
+
+});
